6444b98dd7e3729229a51a79796854d5fb8da2ae
Jagor <96289746+OgurchecInSpace@users.noreply.github.com>
2023-08-23 16:50:54 +0500

    Delete project directory

--- project/app/static/js/error.js
+++
@@ -1,3 +0,0 @@
-// Вставляем в место для ссылки ссылки
-$('ul').html('<li class="link"><a href="/">Main</a></li>' + $('ul').html());
-$('ul').html('<li class="link"><a href="/api">API</a></li>' + $('ul').html());
